8bdd462128303919f28121359a528baaa96f3314
Hristian Iliev <hristian00i@gmail.com>
2023-01-19 14:41:47 +0200

    Add files via upload

---
+++ python/code/p4.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "fruit", "banana"]
+for x in fruits:
+  print(x)
+
+for i in x:
+  if x == 0:
+    x = 6
+
+for y in range(1, x):
+  print(y)
---
+++ python/code/p5.py
@@ -0,0 +1,5 @@
+def addNumbers(num1, num2):
+    sum = num1 + num2
+    return sum
+
+result = addNumbers(5, 4)
